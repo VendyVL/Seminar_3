1b26ba23fd56ac417f70ec28fd8b1fa434d2674b
«VendyVL» <vedmansk@yandex.ru>
2022-05-22 01:18:45 +0300

    задача 23

---
+++ Ex_23/Program23.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int numA = Math.Abs(num);       // Чтобы отрицательные работали
+if (numA > 20) Console.WriteLine("Давай поменьше, в экран не влазит");
+else
+{
+    for (int i = 1; i <= numA; i++)
+    {
+        int res = i * i * i ;
+        Console.Write(res+" ");
+    }
+}
+// оформление как в примере. Куда тут таблицу пихать?
